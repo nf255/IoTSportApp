638ff2a0347423320dcbb00906f59e8a33931827
nf255 <nilsfloss25@gmail.com>
2023-11-14 21:15:39 +0100

    Opencv applied, but image shown as stripes

--- app/src/main/java/com/example/sportapp/MainActivity.kt
+++ app/src/main/java/com/example/sportapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.sportapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.media.Image
+import java.nio.ByteBuffer
 import android.Manifest
+import android.media.ImageReader
+import android.graphics.ImageFormat
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
@@ -8,6 +14,9 @@ import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
+import android.hardware.camera2.params.OutputConfiguration
+import android.hardware.camera2.params.SessionConfiguration
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
@@ -15,6 +24,7 @@ import android.util.Size
 import android.view.Surface
 import android.view.TextureView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -36,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     private var previewSize: Size? = null
     private var mPrevFrame: Mat? = null
     private val motionThreshold: Int = 500
+    private lateinit var imageProcessingThread: HandlerThread
+    private lateinit var imageProcessingHandler: Handler
 
     companion object {
         private const val CAMERA_PERMISSION_REQUEST_CODE = 1
@@ -60,6 +72,9 @@ class MainActivity : AppCompatActivity() {
             setupCamera()
             openCamera()
         }
+        imageProcessingThread = HandlerThread("ImageProcessingThread")
+        imageProcessingThread.start()
+        imageProcessingHandler = Handler(imageProcessingThread.looper)
     }
 
     private fun setupCamera() {
@@ -80,6 +95,7 @@ class MainActivity : AppCompatActivity() {
                 return
             }
             cameraManager.openCamera(cameraId!!, object : CameraDevice.StateCallback() {
+                @RequiresApi(Build.VERSION_CODES.P)
                 override fun onOpened(cameraDevice: CameraDevice) {
                     // Use the cameraDevice here
                     startPreview(cameraDevice)
@@ -98,30 +114,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     private fun startPreview(cameraDevice: CameraDevice) {
-        val surfaceTexture = textureView.surfaceTexture
-        surfaceTexture?.setDefaultBufferSize(previewSize!!.width, previewSize!!.height)
-        val surface = Surface(surfaceTexture)
-
-        try {
-            val previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
-                addTarget(surface)
+        val imageReader = ImageReader.newInstance(previewSize!!.width, previewSize!!.height, ImageFormat.YUV_420_888, 2)
+        imageReader.setOnImageAvailableListener({ reader ->
+            var image: Image? = null
+            try {
+                image = reader.acquireLatestImage()
+                if (image != null) {
+                    val processedFrame = processImage(image)
+                    displayProcessedFrame(processedFrame)
+                    processedFrame.release()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                image?.close()
             }
-            cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(session: CameraCaptureSession) {
-                    // The camera is already closed
+        }, imageProcessingHandler)
 
-                    // When the session is ready, we start displaying the preview.
-                    session.setRepeatingRequest(previewRequestBuilder.build(), null, null)
-                }
-
-                override fun onConfigureFailed(session: CameraCaptureSession) {
-                    Toast.makeText(this@MainActivity, "Failed to configure camera.", Toast.LENGTH_SHORT).show()
-                }
-            }, null)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        val previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+            addTarget(imageReader.surface)
         }
+
+        val sessionStateCallback = object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                session.setRepeatingRequest(previewRequestBuilder.build(), null, null)
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {
+                Toast.makeText(this@MainActivity, "Failed to configure camera.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Using the new method with SessionConfiguration
+        val sessionConfiguration = SessionConfiguration(
+            SessionConfiguration.SESSION_REGULAR,
+            listOf(OutputConfiguration(imageReader.surface)),
+            mainExecutor, // mainExecutor is available on API level 28 and above
+            sessionStateCallback
+        )
+
+        cameraDevice.createCaptureSession(sessionConfiguration)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -132,6 +166,56 @@ class MainActivity : AppCompatActivity() {
                 openCamera()
             } else {
                 Toast.makeText(this, "Camera permission is required", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun processImage(image: Image): Mat {
+        val yuvMat = imageToMat(image)
+        val outMat = Mat()
+
+        // Ensure correct conversion from YUV to grayscale
+        Imgproc.cvtColor(yuvMat, outMat, Imgproc.COLOR_YUV2GRAY_420)
+        yuvMat.release()
+
+        return outMat
+    }
+
+    private fun imageToMat(image: Image): Mat {
+        val planes = image.planes
+        val yPlane = planes[0].buffer
+        val uPlane = planes[1].buffer
+        val vPlane = planes[2].buffer
+
+        val ySize = yPlane.remaining()
+        val uSize = uPlane.remaining()
+        val vSize = vPlane.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        yPlane.get(nv21, 0, ySize)
+        uPlane.get(nv21, ySize, uSize)
+        vPlane.get(nv21, ySize + uSize, vSize)
+
+        val yuvMat = Mat(image.height + image.height / 2, image.width, CvType.CV_8UC1)
+        yuvMat.put(0, 0, nv21)
+
+        return yuvMat
+    }
+
+    private fun displayProcessedFrame(mat: Mat) {
+        val bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(mat, bitmap)
+
+        runOnUiThread {
+            val canvas: Canvas? = textureView.lockCanvas()
+            try {
+                canvas?.drawBitmap(bitmap, 0f, 0f, null)
+            } finally {
+                if (canvas != null) {
+                    textureView.unlockCanvasAndPost(canvas)
+                }
+                bitmap.recycle() // Recycle the bitmap to free memory
             }
         }
     }
